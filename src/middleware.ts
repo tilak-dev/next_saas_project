39b4b0724342122f17e6a0cde8ee393427393d2d
Tilak Singh <tsingh99991@gmail.com>
2024-08-21 09:54:19 +0530

    managed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,38 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware()
+const isPublicRoute = createRouteMatcher(["/signin", "signup", "/", "/home"]);
+const isPublicApi = createRouteMatcher(["/api/videos"]);
+
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  const currentUrl = new URL(req.url);
+  const isHomePage = currentUrl.pathname === "/home";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+  if (userId && isPublicRoute(req) && !isHomePage) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+  //not logged in
+  if (!userId) {
+    //if user is not logged in and tring to access the protected routes
+    if (!isPublicRoute(req) && !isPublicApi(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+    //if the request is for a protected api and the user is not logged in
+    if (isApiRequest && !isPublicRoute(req)) {
+      return NextResponse.redirect(new URL("/signin", req.url));
+    }
+  }
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!.8\\..*|_next).*)",
+    "/",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
